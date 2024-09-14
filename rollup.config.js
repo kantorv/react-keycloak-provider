55c492d0dd41d1b1892d578dffd2afa03f92aedc
Valentin Kantor <kantorvv@gmail.com>
2024-09-14 15:10:37 +0300

    cjs module build error fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import commonjs   from "@rollup/plugin-commonjs";
-import resolve    from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "@rollup/plugin-typescript";
 import postcss    from "rollup-plugin-postcss";
 import url        from '@rollup/plugin-url'
 import dts        from 'rollup-plugin-dts'
+import replace from '@rollup/plugin-replace';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 
 import packageJson from "./package.json";
 
@@ -23,16 +25,27 @@ export default [{
       sourcemap: true
     }
   ],
-  external: ['crypto'], // Mark crypto as external
+
   plugins: [
     peerDepsExternal(),
-    resolve({
-      preferBuiltins: false // Set this to false to avoid the warning
+    nodeResolve({
+      preferBuiltins: true,
+      browser: true
     }),
-    commonjs(), 
+    commonjs({
+      include: /node_modules/, // Convert CommonJS modules to ES6, especially for packages like `keycloak-js`
+    }),
     typescript(),
     postcss(),
     url(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true,
+    }),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**"
+    })
   ]
 },
 {
